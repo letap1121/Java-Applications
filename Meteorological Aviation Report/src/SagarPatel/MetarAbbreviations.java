d482c0d3cadac7b7b54a825f928c5a0a38317b79
letap1121 <sagar2332@icloud.com>
2019-05-25 14:35:10 -0500

    Add files via upload

---
+++ Meteorological Aviation Report/src/SagarPatel/MetarAbbreviations.java
@@ -0,0 +1,43 @@
+package Sagar Patel;
+import java.util.Scanner;
+
+public class MetarAbbreviations {
+
+	public static void main(String[] args){ 
+	Scanner s = new Scanner(System.in);		// Scanner S takes in user input
+	System.out.print("Enter Meter Abbreviation: ");
+	String code = s.nextLine();
+
+	String translation = " ";				// The interpretations of the codes will be stored here.
+
+	if (code.equals("+")) {					// If any of the 8 abbreviations are given in upper or lower case print associated meanings, else unknown
+		translation = "Heavy Intensity";
+	}
+	else if (code.equals("-")) {
+		translation = "Light Intensity";
+	}
+	else if (code.equals("B")||code.equals("b")) {
+		translation = "Began At Time";
+	}
+	else if (code.equals("DZ")||code.contentEquals("dz")) {
+		translation = "Drizzle";
+	}
+	else if (code.equals("E")||code.contentEquals("e")) {
+		translation = "East";
+	}
+	else if (code.equals("HZ")||code.contentEquals("hz")) {
+		translation = "Haze";
+	}
+	else if (code.equals("RA")||code.contentEquals("ra")) {
+		translation = "Rain";
+	}
+	else if (code.equals("SN")||code.contentEquals("sn")) {
+		translation = "Snow";
+	}
+	else {
+		translation = "Unknown Abreviation";
+	}
+	System.out.println(translation);		// Prints the interpretations stored in translation.
+s.close();									// Closes Scanner S
+}
+}
